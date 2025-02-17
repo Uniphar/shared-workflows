543f77d1d62eb65790c02782c2a2a55e2cd38f31
Daniel Braga <96987503+ddbraga@users.noreply.github.com>
2025-02-17 14:00:50 +0000

    Add GitHub Actions workflow for importing APIs to Azure API Management (#1)
    * Add GitHub Actions workflow for importing APIs to Azure API Management
    
    * Remove unnecessary authentication inputs from API import workflow
    
    * Fix parameter passing in API import workflow script
    
    * API name quotes test
    
    * Remove unused parameters from doco
    
    * update example
    
    * Add validation to prevent leading slashes in ApiPath and SpecificationPath parameters
    
    * Rename SpecificationPath parameter to OpenApiSpecPath for clarity
    
    * Rename specificationPath parameter to OpenApiSpecPath in workflow
    
    * Normalize casing of openApiSpecPath parameter in workflow

---
+++ utilities/Import-UniApimApi.ps1
@@ -0,0 +1,73 @@
+function Import-UniApimApi 
+{
+<#
+.SYNOPSIS
+Imports an API onto Azure API Management.
+
+.DESCRIPTION
+Imports an API onto Azure API Management.
+
+.PARAMETER Environment
+The Azure APIM environment.
+
+.PARAMETER ApiName
+The name of the API.
+
+.PARAMETER ApiPath
+The path of the API. Without the leading slash.
+
+.PARAMETER OpenApiSpecPath
+The path to the API specification. Without the leading slash.
+
+.EXAMPLE
+Import-UniApimApi -Environment "dev" -ApiName "my-api" -ApiPath "my-api" -OpenApiSpecPath "my-api/swagger.json"
+
+.NOTES
+At the moment this function only supports scenarios where the following is true:
+- The API is hosted under the "API" subdomain of one of the uniphar.ie zones.
+- The API is imported as an OpenAPI specification.
+- The API is imported to the "Development" product.
+#>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $true, Position = 0)]
+        [ValidateSet('dev', 'test', 'prod')]
+        [string]$Environment,
+
+        [parameter(Mandatory = $true, Position = 1)]
+        [string]$ApiName,
+
+        [parameter(Mandatory = $true, Position = 2)]
+        [ValidatePattern('^[^/]')] # No leading slash
+        [string]$ApiPath,
+
+        [parameter(Mandatory = $true, Position = 3)]
+        [ValidatePattern('^[^/]')] # No leading slash
+        [string]$OpenApiSpecPath
+    )
+
+    $ResourceGroupName = "web-$Environment"
+    $ApiManagementName = "$ResourceGroupName-apim"
+
+    switch ($Environment) {
+        'prod' { $apiHostname = "api.uniphar.ie" }
+        default { $apiHostname = "api.$Environment.uniphar.ie" }
+    }
+
+    $serviceUrl = "https://$apiHostname"
+    $specificationUrl = "$serviceUrl/$OpenApiSpecPath"
+    
+    $context = New-AzApiManagementContext -ResourceGroupName $ResourceGroupName -ServiceName $ApiManagementName
+
+    $apiId = (Get-AzApiManagementApi -Context $context -Name $ApiName -ErrorAction SilentlyContinue).ApiId
+    if ($null -eq $apiId) {
+        $api = Import-AzApiManagementApi -Context $context -SpecificationFormat OpenApi -SpecificationUrl $specificationUrl -Path $ApiPath -ServiceUrl $serviceUrl
+    }
+    else{
+        $api = Import-AzApiManagementApi -Context $context -ApiId $apiId -SpecificationFormat OpenApi -SpecificationUrl $specificationUrl -Path $ApiPath -ServiceUrl $serviceUrl
+    }
+
+    $product = Get-AzApiManagementProduct -Context $context -Title 'Development'
+
+    Add-AzApiManagementApiToProduct -Context $context -ProductId $product.ProductId -ApiId $api.ApiId
+}
